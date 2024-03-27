a06f2b55b2f43c87e510afd4349d436e59cb4e89
AdishtJaglan <adisht7524@gmail.com>
2024-03-27 12:11:32 +0530

    set up game board

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("Connected!");
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    const getBoard = () => board;
+
+    return { board };
+}
