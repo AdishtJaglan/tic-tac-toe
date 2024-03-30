e2608505be209a05b52b04dbf1eb3ca144a8c0ef
AdishtJaglan <adisht7524@gmail.com>
2024-03-31 02:03:25 +0530

    refactor checkWinner function to improve readability

--- script.js
+++ script.js
@@ -62,28 +62,30 @@ const gameController = (playerOneName = "Player One", playerTwoName = "Player Tw
         activePlayer = activePlayer === player[0] ? player[1] : player[0];
     };
 
+    const checkDiagnolWin = (gameBoard) => {
+        return (gameBoard[0][0] !== 0 && gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2]) ||
+            (gameBoard[0][2] !== 0 && gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0]);
+    };
+
+    const checkRowWin = (gameBoard, i) => {
+        return gameBoard[i][0] !== 0 && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2];
+    };
+
+    const checkColumnWin = (gameBoard, i) => {
+        return gameBoard[0][i] !== 0 && gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i];
+    };
+
     const checkWinner = () => {
         const gameBoard = board.getBoard();
 
         /* diagnol win conditiion */
-        if (
-            (gameBoard[0][0] !== 0 && gameBoard[0][0] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][2]) ||
-            (gameBoard[0][2] !== 0 && gameBoard[0][2] === gameBoard[1][1] && gameBoard[1][1] === gameBoard[2][0])
-        ) {
+        if (checkDiagnolWin(gameBoard)) {
             return 1;
         }
 
         /* checking row and column win condition */
         for (let i = 0; i < 3; i++) {
-            if (
-                gameBoard[i][0] !== 0 && gameBoard[i][0] === gameBoard[i][1] && gameBoard[i][1] === gameBoard[i][2]
-            ) {
-                return 1;
-            }
-
-            if (
-                gameBoard[0][i] !== 0 && gameBoard[0][i] === gameBoard[1][i] && gameBoard[1][i] === gameBoard[2][i]
-            ) {
+            if (checkColumnWin(gameBoard, i) || checkRowWin(gameBoard, i)) {
                 return 1;
             }
         }
